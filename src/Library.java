d97d507890a5da5106568178d9e3f2150386e537
kv-zayceva <115557216+kv-zayceva@users.noreply.github.com>
2023-03-13 13:52:11 +0300

    es
    Vnesla esp

--- src/Library.java
+++ src/Library.java
@@ -12,20 +12,20 @@ public class Library {
     }
 
     public void addBook() {
-        System.out.println("Введите название книги:");
+        System.out.println("Ingresa el título del libro:");
         String name = scanner.nextLine();
 
-        System.out.println("Введите автора книги:");
+        System.out.println("Ingresa el autor o la autora del libro:");
         String author = scanner.nextLine();
 
-        System.out.println("Введите год издания:");
+        System.out.println("Ingresa el año de publicación:");
         int year = scanner.nextInt();
 
         bookList.add(new Book(name, author, year));
     }
 
     public void findByAuthor() {
-        System.out.println("Введите автора:");
+        System.out.println("Ingresa el autor o la autora del libro:");
         String author = scanner.nextLine();
 
         for (Book book : bookList) {
@@ -36,7 +36,7 @@ public class Library {
     }
 
     public void printAll() {
-        System.out.println("Всего найдено: " + bookList.size() + " книг.");
+        System.out.println("Un total de " + bookList.size() + " libros encontrados.");
         for (Book book : bookList) {
             System.out.println(book.getAuthor() + " " + book.getTitle() + ", " + book.getYear());
         }
